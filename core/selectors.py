79aca5522926f6f7f635898998bea6d1be9f2fcd
bjeffries <bill@bluecrabtech.net>
2015-01-07 07:16:37 -0500

    Added roulette wheel selector

--- core/selectors.py
+++ core/selectors.py
@@ -5,22 +5,19 @@
     input: population of chromosomes
     output: pairs of selected parent chromosomes
 
+    types:
+        RankSelector: only selects parents with fitness rank above specified threshold
+        RouletteSelector: uses probabilistic model to select parents based on relative fitness
 """
 import random
+import numpy as np
 
-"""
-    Base class, not to be used
 
-"""
 class SelectionOperator:
     def select_pairs(self, population):
         pass
 
-"""
-    RankSelector:
-    - only selects parents with fitness rank above specified threshold
 
-"""
 class RankSelector(SelectionOperator):
     def __init__(self, threshold):
         self._threshold = threshold
@@ -42,6 +39,48 @@ class RankSelector(SelectionOperator):
             random_2 = random.randint(0, len(parent_pool)-1)
             parent_1 = parent_pool[random_1]
             parent_2 = parent_pool[random_2]
+            parents.append([parent_1, parent_2])
+
+        return parents
+
+
+class RouletteSelector(SelectionOperator):
+    def select_pairs(self, population):
+        # create wheel points
+        size = len(population)
+        pointer_size = 1.0 / float(size)
+        spin = random.random() * pointer_size
+        wheel_pointers = np.empty(size)
+        wheel_pointers.fill(pointer_size)
+        wheel_pointers = np.multiply(wheel_pointers[0], range(size))
+        wheel_pointers = np.add(wheel_pointers, spin)
+        wheel_pointers = np.reshape(wheel_pointers, (size, 1))
+
+        # create wheel spaces
+        fitness = []
+        for chrom in population:
+            fitness.append(chrom.fitness)
+        total_fitness = np.sum(fitness)
+        norm_fitness = np.divide(fitness, total_fitness)
+        current_pointer = 0
+        wheel_spaces = []
+        for f in norm_fitness:
+            space = f + current_pointer
+            wheel_spaces.append(space)
+            current_pointer = space
+
+        # select from wheel
+        parent_pool = []
+        wheel_selections = np.less_equal(wheel_pointers, wheel_spaces)
+        for w in wheel_selections:
+            index = w.argmax()
+            parent_pool.append(population[index])
+
+        # pair off parents
+        parents = []
+        for i in range(0, size, 2):
+            parent_1 = parent_pool[i]
+            parent_2 = parent_pool[i+1]
             parents.append([parent_1, parent_2])
 
         return parents
--- tests/selector_tests.py
+++ tests/selector_tests.py
@@ -9,28 +9,56 @@ Create population of 100 individuals with random fitness levels
 Verify that RankSelector selects the top 50
 
 """
-# initialize population
-pop_size = 100
-population = []
-for p in range(pop_size):
-    c = chrom.Chromosome(genes=[])
-    fitness = random.randint(0, 100)
-    c.fitness = fitness
-    population.append(c)
+def test_rank_selector():
+    print("Testing RankSelector")
+    # initialize population
+    pop_size = 100
+    population = []
+    for p in range(pop_size):
+        c = chrom.Chromosome(genes=[])
+        fitness = random.randint(0, 100)
+        c.fitness = fitness
+        population.append(c)
 
-# get parents using RankSelector
-rank_selector = selector.RankSelector(threshold=50)
-parents = rank_selector.select_pairs(population=population)
+    # get parents using RankSelector
+    rank_selector = selector.RankSelector(threshold=50)
+    parents = rank_selector.select_pairs(population=population)
 
-# verify 50 parents returned
-assert len(parents) == pop_size/2
+    # verify 50 parents returned
+    assert len(parents) == pop_size/2
 
-# verify top 50 returned
-population.sort(key=lambda x: x.fitness, reverse=True)
-fitness_threshold = population[int(pop_size/2)-1].fitness
-for p in parents:
-    for c in p:
-        assert c.fitness >= fitness_threshold
+    # verify top 50 returned
+    population.sort(key=lambda x: x.fitness, reverse=True)
+    fitness_threshold = population[int(pop_size/2)-1].fitness
+    for p in parents:
+        for c in p:
+            assert c.fitness >= fitness_threshold
+    print("RankSelector passed")
+
+""" Test RouletteSelector
+Create population of 100 individuals with random fitness levels
+
+"""
+def test_roulette_selector():
+    print("Testing RouletteSelector")
+    # initialize population
+    pop_size = 100
+    population = []
+    for p in range(pop_size):
+        c = chrom.Chromosome(genes=[])
+        fitness = random.randint(0, 100)
+        c.fitness = fitness
+        population.append(c)
+
+    # get parents using RouletteSelector
+    rank_selector = selector.RouletteSelector()
+    parents = rank_selector.select_pairs(population=population)
+
+    # verify 50 parents returned
+    assert len(parents) == pop_size/2
+
+    print("RouletteSelector passed")
 
 
-print("Selector tests PASS")
+#test_rank_selector()
+test_roulette_selector()
